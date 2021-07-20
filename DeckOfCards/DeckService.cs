0f8e35b17707a5fb8c2305aca54fcd85f8ab26fa
manibushan24 <madishettymanibushan@gmail.com>
2021-07-20 14:53:51 -0600

    Test

--- DeckOfCards/DeckService.cs
+++ DeckOfCards/DeckService.cs
@@ -25,5 +25,10 @@ namespace DeckofCards.Service
                    .ToList();
             return Cards;
         }
+
+        public string test()
+        {
+            return "hi";
+        }
     }
 }
